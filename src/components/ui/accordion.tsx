e8339d78e9525bf0d231d84e07c0c6d2aafa4851
Bikesh Kumar <bikeshkumar0505@gmail.com>
2025-09-18 18:58:52 +0000

    make editing canvas more professional and advanced

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -14,7 +14,7 @@ const AccordionItem = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
-    className={cn("border-b-0", className)}
+    className={cn("border-b", className)}
     {...props}
   />
 ))
@@ -28,12 +28,13 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:no-underline",
+        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:no-underline [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
     >
       {children}
+      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
