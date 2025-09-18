6640163a7b9d5d9905d98886992e94e33fd6b9d2
Bikesh Kumar <bikeshkumar0505@gmail.com>
2025-09-18 19:07:20 +0000

    move image logo slightly right more and reduce its transparency

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -166,7 +166,7 @@ export function AppHeader({
                     </Button>
                 ) : (
                      <Link href="/" className="relative flex items-center h-8 w-24">
-                        <Image src="/camly.png" alt="Camly logo" width={28} height={28} className="absolute top-0.5 left-1" />
+                        <Image src="/camly.png" alt="Camly logo" width={28} height={28} className="absolute top-0.5 left-2 opacity-100" />
                         <h1 className="text-xl font-bold hidden sm:inline-block relative left-7">Favicon</h1>
                     </Link>
                 )}
