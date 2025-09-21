ed479778b3f33a522940537825328d185b350ccb
Bikesh Kumar <bikeshkumar0505@gmail.com>
2025-09-21 21:00:38 +0000

    add google analytics code in layout , <!-- Google tag (gtag.js) --> <scr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { Footer } from '@/components/footer';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: {
@@ -62,6 +63,15 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#F2683C" />
       </head>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-KLK9J0JQR0"></Script>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KLK9J0JQR0');
+        `}
+      </Script>
       <body className="font-body antialiased flex flex-col min-h-screen">
           <div className="flex-grow">
             {children}
