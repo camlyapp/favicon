c8b30b9be51d39ab6add5a7a7e04082cb1762564
Bikesh Kumar <bikeshkumar0505@gmail.com>
2025-09-21 19:52:32 +0000

    sitemap couldnt fetching by google

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next';
+
+const URL = 'https://favicon-generate.vercel.app';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${URL}/`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${URL}/editor`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ];
+}
